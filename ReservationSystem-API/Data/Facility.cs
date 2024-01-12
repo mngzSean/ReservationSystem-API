fafb807db6342d8f8d03d1a00ca9f03c1f1af8ba
mngz <mngz2537@gmail.com>
2024-01-13 04:12:35 +0900

    facility 테이블 생성할 때 만들어지는 마이그레이션 파일 및 수행 확인
    add-migration "init facility"
    update-database

--- ReservationSystem-API/Data/Facility.cs
+++ ReservationSystem-API/Data/Facility.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Reflection.Metadata.Ecma335;
 
 namespace ReservationSystem_API.Data
 {
     public class Facility
     {
+        [Key]
         public int Idx { get; set; }
         public string Name { get; set; }
         public string Purpose{ get; set; }
---
+++ ReservationSystem-API/Migrations/20240112190823_init facility.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ReservationSystem_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class initfacility : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "facilities",
+                columns: table => new
+                {
+                    idx = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    purpose = table.Column<string>(type: "text", nullable: false),
+                    location = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_facilities", x => x.idx);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "facilities");
+        }
+    }
+}
