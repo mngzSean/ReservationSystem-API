cc3299f69bb414dd59db8aaf13ebe872ab4e6453
mngz <mngz2537@gmail.com>
2024-01-13 04:06:04 +0900

    facility 테이블 생성하는 코드 추가

---
+++ ReservationSystem-API/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReservationSystem_API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // connect to postgres with connection string from app settings
+            optionsBuilder
+                .UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"))
+                .UseSnakeCaseNamingConvention();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // idx colume을 GENERATED ALWAYS AS IDENTITY(auto-increment) 값으로 변경
+            modelBuilder.Entity<Facility>().Property(b => b.Idx).UseIdentityAlwaysColumn();     
+        }
+
+        public DbSet<Facility> Facilities { get; set; }
+    }
+}
---
+++ ReservationSystem-API/Data/Facility.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ReservationSystem_API.Data
+{
+    public class Facility
+    {
+        public int Idx { get; set; }
+        public string Name { get; set; }
+        public string Purpose{ get; set; }
+        public string Location { get; set; }
+    }
+}
